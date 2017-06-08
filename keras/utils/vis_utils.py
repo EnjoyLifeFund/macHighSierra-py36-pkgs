6537a07b63920a1d38cd0f3e4a11ade4cb2bd28e
ralic <raliclo@gmail.com>
2017-06-08 12:01:58 -0700

    base update

--- keras/utils/vis_utils.py
+++ keras/utils/vis_utils.py
@@ -13,7 +13,7 @@ except ImportError:
 
 
 def _check_pydot():
-    if not (pydot and pydot.find_graphviz()):
+    if not (pydot): ## and pydot.find_graphviz()
         raise ImportError('Failed to import pydot. You must install pydot'
                           ' and graphviz for `pydotprint` to work.')
 
